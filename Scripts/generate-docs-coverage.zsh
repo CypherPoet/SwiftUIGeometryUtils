6eaf0b90f34c70a04bd8dc214b5a28a3610b8b93
CypherPoet <CypherPoet@gmail.com>
2021-05-18 06:43:45 -0500

    Switch to swift-doc for generating documentation

---
+++ Scripts/generate-docs-coverage.zsh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+swift-doc coverage ./Sources/SwiftUIGeometryUtils --output "dcov.json"
---
+++ Scripts/generate-html-docs.zsh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+swift-doc generate ./Sources/SwiftUIGeometryUtils --module-name SwiftUIGeometryUtils --format html --base-url "./"
