facdf66f5e1fd8bc48ce32fd9e0037359ddf87c6
CypherPoet <CypherPoet@gmail.com>
2021-05-18 06:35:40 -0500

    Add `Angle.straightAngle` and  `Angle.rightAngle` constants.

---
+++ Sources/SwiftUIGeometryUtils/Extensions/Angle/Angle+Constants.swift
@@ -0,0 +1,11 @@
+//
+// Angle+Constants.swift
+//
+
+import SwiftUI
+
+
+extension Angle {
+    public static let straightAngle = Self.radians(.pi)
+    public static let rightAngle = Self.straightAngle / 2
+}
